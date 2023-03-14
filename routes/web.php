b9aa3b2a86583e1d576e30c91522cb065a6851fb
Mohammed Naji <m.naji1512@gmail.com>
2023-03-14 16:50:04 +0200

    lec6 - routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function PHPUnit\Framework\returnSelf;
+
+// use Illuminate\Support\Facades\Route;
+
 // class Person {
 
 //     public  static function printName() {
@@ -25,10 +29,91 @@ use Illuminate\Support\Facades\Route;
 // use , namespace
 
 
-Route::get('/', function() {
-    return 'Homepage new';
+// Route::get('/', function() {
+//     return 'Homepage new';
+// });
+
+// Route::get('/about', function() {
+//     return 'About new';
+// });
+
+// Route::get('url', 'Action');
+// Route::post('', '');
+// Route::put('', '');
+// Route::patch('', '');
+// Route::delete('', '');
+
+Route::post('/', function() {
+    return 'Homepage Page - Post';
 });
 
-Route::get('/about', function() {
-    return 'About new';
+Route::put('/', function() {
+    return 'Homepage Page - Put';
 });
+
+Route::delete('/', function() {
+    return 'Homepage Page - Delete';
+});
+
+Route::get('/', function() {
+    return 'Homepage Page - Get';
+});
+
+// anonymous, callback, clouser
+
+// function sayHi () {
+//     echo
+//     return
+// }
+
+// function vat ($price) {
+//     return $price * .15;
+// }
+
+// $vat = vat(100);
+
+// echo $price + $vat;
+
+// Route::match(['patch', 'put'], 'privacy', function() {
+//     return 'Done';
+// });
+
+// Route::any('contact', function() {
+//     return 'Any';
+// });
+
+// Route::get('/user/{name}', function($name) {
+//     return 'Welcome ' . $name;
+// });
+
+Route::get('sessions/{course}/{type?}', function ($course, $type = '') {
+    return 'Course ' . $course . ' ' . $type;
+});
+
+
+Route::prefix('admin')->group(function() {
+    Route::get('/home', function() { return 'Admin Home'; });
+    Route::get('/posts', function() { return 'Admin Posts'; });
+    Route::get('/products', function() { return 'Admin Products'; });
+    Route::get('/orders', function() { return 'Admin Orders'; });
+});
+
+// Route::fallback(function() {
+//     return 'فش صفحة ي حبيبي';
+// });
+
+// Route::get('/{ddd}', function() {
+//     return 'Error';
+// });
+
+// Route::get('/{ddd}/{dd}/{d}/{de}', function() {
+//     return 'Error';
+// });
+
+// Route::get('/{ddd}/{re}/{qq}', function() {
+//     return 'Error';
+// });
+
+// Route::get('/{ddd}/{qq}', function() {
+//     return 'Error';
+// });
